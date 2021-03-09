00a2fa5e210032f896317ea84b88e69cce1b6f30
unfunuk <a.s.s.t.e.r.i.x.0@gmail.com>
2021-03-09 18:52:14 +0300

    feat: fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse(n) {
-    return n = Number(String(n).split('').reverse().join(''));
+    module.exports = function reverse(n) {
+        if (n < 0) {
+            n *= -1;
+        }
+        let m = String(n);
+        let k = m.split("").reverse().join("");
+        let j = Number(k);
+        return j;
+    }
 }
